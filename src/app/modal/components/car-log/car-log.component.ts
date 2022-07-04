5733707fa3b8fda98c2d2e48d71c72f86b635edc
VolodumurGruv <gru.v@i.ua>
2022-07-04 05:05:27 +0300

    updated

--- src/app/modal/components/car-log/car-log.component.ts
+++ src/app/modal/components/car-log/car-log.component.ts
@@ -9,6 +9,12 @@ export class CarLogComponent implements OnInit {
   public carModel: string[] = ['BMW | X3', 'AUDI | A4'];
   public numbers: string[] = ['AA1212AA', 'BB2323BB'];
 
+  public improvedCarModel: any[] = [
+    { 'BMW | X3': 'BB2323BB', 'AUDI | A4': 'AA1212AA' },
+  ];
+
+  public selectedCar!: string;
+
   constructor() {}
 
   ngOnInit(): void {}
